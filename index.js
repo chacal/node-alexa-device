2d3a061ad558369e0cb7ca01c2534eec609b8945
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-10-20 23:09:15 +0300

    Use refresh-token module to get access token

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const streamBuffers = require('stream-buffers')
 const player = require('play-sound')()
 const record = require('node-record-lpcm16')
 const {Detector, Models} = require('snowboy')
+const BPromise = require('bluebird')
+const TokenProvider = require('refresh-token')
 
 const AVS_API_URL = 'https://avs-alexa-na.amazon.com/v20160207'
-const ACCESS_TOKEN = JSON.parse(fs.readFileSync('./access_token.json')).token
+const AVS_CREDENTIALS = JSON.parse(fs.readFileSync('./avs-credentials.json'))
+const tokenProvider = BPromise.promisifyAll(new TokenProvider('https://api.amazon.com/auth/o2/token', AVS_CREDENTIALS))
 
 
 registerForDirectives()
-startWakeWordDetection()
+  .then(startWakeWordDetection)
 
 
 function startWakeWordDetection() {
@@ -48,28 +51,30 @@ function startWakeWordDetection() {
 
 
 function sendSpeechRequest() {
-  const req = http.request(_.assign({}, url.parse(AVS_API_URL + '/events'), {
-      method: 'POST',
-      headers: {
-        authorization: 'Bearer ' + ACCESS_TOKEN,
-        'content-type': 'multipart/form-data; boundary=this-is-a-boundary'
-      }
-    })
-  )
+  return tokenProvider.getTokenAsync()
+    .then(accessToken => {
+      const req = http.request(_.assign({}, url.parse(AVS_API_URL + '/events'), {
+          method: 'POST',
+          headers: {
+            authorization: 'Bearer ' + accessToken,
+            'content-type': 'multipart/form-data; boundary=this-is-a-boundary'
+          }
+        })
+      )
 
-  req.on('response', function(response) {
-    console.log('Got streaming response', response.headers)
-    handleResponse(response)
-    response.on('end', startWakeWordDetection)
-  })
+      req.on('response', function(response) {
+        console.log('Got streaming response', response.headers)
+        handleResponse(response)
+        response.on('end', startWakeWordDetection)
+      })
 
-  req.on('error', function(response) {
-    console.log('ERROR')
-    response.pipe(process.stdout)
-    response.on('end', () => console.log('All done!'))
-  })
+      req.on('error', function(response) {
+        console.log('ERROR')
+        response.pipe(process.stdout)
+        response.on('end', () => console.log('All done!'))
+      })
 
-  req.write(`--this-is-a-boundary
+      req.write(`--this-is-a-boundary
 Content-Disposition: form-data; name="metadata"
 Content-Type: application/json; charset=UTF-8
 
@@ -93,34 +98,38 @@ Content-Type: application/octet-stream
 
 `)
 
-  const recording = record.start()
-  recording.on('data', data => req.write(data))
-  recording.on('end', data => {
-      console.log('Recording ended.')
-      req.write(`--this-is-a-boundary--`)
-      req.end()
-    }
-  )
+      const recording = record.start()
+      recording.on('data', data => req.write(data))
+      recording.on('end', data => {
+          console.log('Recording ended.')
+          req.write(`--this-is-a-boundary--`)
+          req.end()
+        }
+      )
+    })
 }
 
 
 
 
 function registerForDirectives() {
-  const directivesReq = http.request(_.assign({}, url.parse(AVS_API_URL + '/directives'), {
-      method: 'GET',
-      headers: {
-        authorization: 'Bearer ' + ACCESS_TOKEN
-      }
+  return tokenProvider.getTokenAsync()
+    .then(accessToken => {
+      const directivesReq = http.request(_.assign({}, url.parse(AVS_API_URL + '/directives'), {
+          method: 'GET',
+          headers: {
+            authorization: 'Bearer ' + accessToken
+          }
+        })
+      )
+
+      directivesReq.on('response', function(response) {
+        console.log('Got directives response', response.headers)
+        handleResponse(response)
+      })
+
+      directivesReq.end()
     })
-  )
-
-  directivesReq.on('response', function(response) {
-    console.log('Got directives response', response.headers)
-    handleResponse(response)
-  })
-
-  directivesReq.end()
 }
 
 
@@ -135,39 +144,50 @@ function uuid() {
 
 
 function handleResponse(response) {
-  var form = new multiparty.Form()
-  form.on('error', function(err) {
-    console.log('Error parsing response', err)
-  })
+  const contentType = response.headers['content-type']
+  if(contentType.indexOf('multipart/') > -1) {
+    var form = new multiparty.Form()
+    form.on('error', function(err) {
+      console.log('Error parsing response', err)
+    })
 
-  form.on('part', function(part) {
+    form.on('part', function(part) {
+      const buf = new streamBuffers.WritableStreamBuffer()
+      part.pipe(buf)
+      part.on('end', () => {
+        switch(part.headers['content-type']) {
+          case 'application/json; charset=UTF-8':
+            console.log('Got JSON:', JSON.stringify(JSON.parse(buf.getContentsAsString('utf8')), null, 2))
+            break;
+          case 'application/json':
+            console.log('Got JSON:', JSON.stringify(JSON.parse(buf.getContentsAsString('utf8')), null, 2))
+            record.stop()
+            break;
+          case 'application/octet-stream':
+            //console.log('Got binary data, length: ', buf.getContents().length)
+            fs.writeFileSync('./output.mp3', buf.getContents())
+            player.play('./output.mp3')
+            break;
+          default:
+            console.log('Got ' + part.headers['content-type'], buf.getContents().length)
+            break;
+        }
+      })
+
+      part.on('error', function(err) {
+        console.log("Error on part " + part)
+        part.resume()
+      })
+    })
+
+    form.parse(response)
+  } else if(contentType.indexOf('application/json') > -1) {
     const buf = new streamBuffers.WritableStreamBuffer()
-    part.pipe(buf)
-    part.on('end', () => {
-      switch(part.headers['content-type']) {
-        case 'application/json; charset=UTF-8':
-          console.log('Got JSON:', JSON.stringify(JSON.parse(buf.getContentsAsString('utf8')), null, 2))
-          break;
-        case 'application/json':
-          console.log('Got JSON:', JSON.stringify(JSON.parse(buf.getContentsAsString('utf8')), null, 2))
-          record.stop()
-          break;
-        case 'application/octet-stream':
-          //console.log('Got binary data, length: ', buf.getContents().length)
-          fs.writeFileSync('./output.mp3', buf.getContents())
-          player.play('./output.mp3')
-          break;
-        default:
-          console.log('Got ' + part.headers['content-type'], buf.getContents().length)
-          break;
-      }
+    response.pipe(buf)
+    response.on('end', () => {
+      console.log('Got JSON:', JSON.stringify(JSON.parse(buf.getContentsAsString('utf8')), null, 2))
     })
-
-    part.on('error', function(err) {
-      console.log("Error on part " + part)
-      part.resume()
-    })
-  })
-
-  form.parse(response)
+  } else {
+    console.log('Unknown content type:', contentType)
+  }
 }
