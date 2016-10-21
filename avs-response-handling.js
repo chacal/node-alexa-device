709caddb0ceb99c5d8d43208386ae5d4e8777da5
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-10-21 18:24:31 +0300

    Separate response parsing to avs-response-handling.js

---
+++ avs-response-handling.js
@@ -0,0 +1,65 @@
+const _ = require('lodash')
+const multiparty = require('multiparty')
+const streamBuffers = require('stream-buffers')
+
+function AvsResponseHandler(onDirectiveCallback, onAudioCallback) {
+  this.onDirectiveCallback = onDirectiveCallback
+  this.onAudioCallback = onAudioCallback
+}
+
+AvsResponseHandler.prototype.handleResponse = function (response) {
+  const self = this
+  const contentType = response.headers['content-type'] || ""
+
+  if(isMultipart()) {
+    handleMultipartResponse(response)
+  } else if(isJson()) {
+    handleJsonResponse(response)
+  } else if(isOctetStream()) {
+    handleBinaryResponse(response)
+  } else {
+    console.log(`Unknown content type: ${contentType}, status: ${response.statusCode}, headers: ${response.headers}`)
+  }
+
+
+  function handleMultipartResponse(response) {
+    var form = new multiparty.Form()
+    form.on('error', function(err) { console.log('Error parsing response', err) })
+    form.on('part', function(part) {
+      AvsResponseHandler.prototype.handleResponse.call(self, part)
+    })
+
+    form.parse(response)
+  }
+
+  function handleJsonResponse(response) {
+    bufferResponse(response, buf => handleJsonMessage(JSON.parse(buf)))
+
+    function handleJsonMessage(message) {
+      if(message.directive) {
+        self.onDirectiveCallback(message.directive)
+      } else {
+        console.log('Got unknown JSON message!', JSON.stringify(message, null, 2))
+      }
+    }
+  }
+
+  function handleBinaryResponse(response) {
+    bufferResponse(response, buf => self.onAudioCallback(buf))
+  }
+
+
+  function isMultipart() { return _.includes(contentType, 'multipart/') }
+  function isJson() { return _.includes(contentType, 'application/json') }
+  function isOctetStream() { return _.includes(contentType, 'application/octet-stream') }
+}
+
+
+function bufferResponse(response, callback) {
+  const buf = new streamBuffers.WritableStreamBuffer()
+  response.pipe(buf)
+  response.on('end', () => callback(buf.getContents()))
+}
+
+
+module.exports = AvsResponseHandler
--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const http = require('http2')
 const url = require('url')
 const _ = require('lodash')
 const fs = require('fs')
-const multiparty = require('multiparty')
-const streamBuffers = require('stream-buffers')
 const player = require('play-sound')()
 const record = require('node-record-lpcm16')
 const BPromise = require('bluebird')
 const TokenProvider = require('refresh-token')
 const WakeWordDetector = require('./wakeword-detector.js')
+const AvsResponseHandler = require('./avs-response-handling.js')
 
 const AVS_API_URL = 'https://avs-alexa-na.amazon.com/v20160207'
 const AVS_CREDENTIALS = JSON.parse(fs.readFileSync('./avs-credentials.json'))
 const tokenProvider = BPromise.promisifyAll(new TokenProvider('https://api.amazon.com/auth/o2/token', AVS_CREDENTIALS))
 
 const wakeWordDetector = new WakeWordDetector()
+const avsResponseHandler = new AvsResponseHandler(handleDirective, handleAudio)
 
 registerForDirectives()
   .then(() => wakeWordDetector.start(sendSpeechRequest))
@@ -30,7 +30,7 @@ function sendSpeechRequest() {
       streamAudioFromMic(req)
 
       req.on('response', function(response) {
-        handleResponse(response)
+        avsResponseHandler.handleResponse(response)
         response.on('end', () => wakeWordDetector.start(sendSpeechRequest))
       })
 
@@ -45,77 +45,31 @@ function registerForDirectives() {
   return tokenProvider.getTokenAsync()
     .then(accessToken => {
       avsGET('/directives', accessToken).on('response', function(response) {
-        handleResponse(response)
+        avsResponseHandler.handleResponse(response)
       })
     })
 }
 
 
-function handleResponse(response) {
-  const contentType = response.headers['content-type'] || ""
-
-  if(isMultipart()) {
-    handleMultipartResponse(response)
-  } else if(isJson()) {
-    handleJsonResponse(response)
-  } else if(isOctetStream()) {
-    handleBinaryResponse(response)
+function handleDirective(directive) {
+  if(directive.header.name === 'StopCapture') {
+    onStopCaptureDirective()
+  } else if(directive.header.name === 'Speak') {
+    // Don't need to handle, binary content will be played back automatically when it arrives
   } else {
-    console.log(`Unknown content type: ${contentType}, status: ${response.statusCode}, headers: ${response.headers}`)
-  }
-
-  function isMultipart() { return _.includes(contentType, 'multipart/') }
-  function isJson() { return _.includes(contentType, 'application/json') }
-  function isOctetStream() { return _.includes(contentType, 'application/octet-stream') }
-}
-
-function handleMultipartResponse(response) {
-  var form = new multiparty.Form()
-  form.on('error', function(err) {
-    console.log('Error parsing response', err)
-  })
-
-  form.on('part', function(part) {
-    handleResponse(part)
-  })
-
-  form.parse(response)
-}
-
-function handleJsonResponse(response) {
-  const buf = new streamBuffers.WritableStreamBuffer()
-  response.pipe(buf)
-  response.on('end', () => {
-    handleJsonMessage(JSON.parse(buf.getContentsAsString('utf8')))
-  })
-}
-
-function handleBinaryResponse(response) {
-  response.pipe(fs.createWriteStream('./output.mp3'))
-  response.on('end', () => player.play('./output.mp3'))
-}
-
-
-
-function handleJsonMessage(message) {
-  if(message.directive) {
-    handleDirective(message.directive)
-  } else {
-    console.log('Got unknown JSON message!', JSON.stringify(message, null, 2))
-  }
-
-  function handleDirective(directive) {
-    if(directive.header.name === 'StopCapture') {
-      onStopCaptureDirective()
-    } else if(directive.header.name === 'Speak') {
-      // Don't need to handle, binary content will be played back automatically when it arrives
-    } else {
-      console.log('Got unknown directive!', JSON.stringify(directive, null, 2))
-    }
+    console.log('Got unknown directive!', JSON.stringify(directive, null, 2))
   }
 }
 
 function onStopCaptureDirective() { record.stop() }
+
+
+
+function handleAudio(audioContent) {
+  fs.writeFileSync('./output.mp3', audioContent)
+  player.play('./output.mp3')
+}
+
 
 
 function avsPOSTMultipart(path, boundary, accessToken) {
