f86c0a34c3f8e598625a2098dc14cb539da33c13
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-01-21 14:28:01 +0200

    Add more gain for the microphone

--- wakeword-detector.js
+++ wakeword-detector.js
@@ -26,7 +26,7 @@ WakeWordDetector.prototype.start = function(wakeWordDetectedCallback) {
     wakeWordDetectedCallback(mic)
   })
 
-  const mic = record.start({ threshold: 0, gain: 10 })
+  const mic = record.start({ threshold: 0, gain: 20 })
   mic.pipe(detector)
 }
 
